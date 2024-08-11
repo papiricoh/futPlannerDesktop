e95521566ff61c99908ae6ebc999ba0ef052d05a
papiricoh <slendeman0200@gmail.com>
2024-08-11 18:53:51 +0200

    added font and structure

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,30 +3,26 @@ import EditSVG from '@/assets/edit.svg'
 import Menhera from '@/assets/menhera.png'
 import Image from 'next/image'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons/index'
+import { faSoccerBall } from '@fortawesome/free-solid-svg-icons/index'
+
 export default function Home() {
 	return (
 		<div className={styles.wrapper}>
+			<nav className={styles.nav}>
+				<div className={styles.navButton}>
+					<FontAwesomeIcon icon={faHome} className={styles.icon} />
+					<div>Resumen</div>
+				</div>
+				<div className={styles.navButton}>
+					<FontAwesomeIcon icon={faSoccerBall} className={styles.icon} />
+					<div>Alineacion</div>
+				</div>
+
+			</nav>
 			<main className={styles.main}>
-				<div className={styles.header}>
-					<h1 className={styles.headerText}>NextJS + Electron Boilerplate</h1>
-					<h4 className={styles.headerSubText}>
-						That one boilerplate you couldn{"'"}t find, until now.
-					</h4>
-				</div>
-				<Image
-					src={Menhera.src}
-					className={styles.menhera}
-					alt="menhera"
-					width={200}
-					height={200}
-				/>
-				<div className={styles.note}>
-					<EditSVG />
-					Get started by editting frontend/app/page.tsx
-				</div>
-				<footer className={styles.footer}>
-					{'<'}/{'>'} with ♥
-				</footer>
+				
 			</main>
 		</div>
 	)
